ccb12fe802500185e1c4bb03c16377f14c25c276
Pader Xiong <paderx26@gmail.com>
2024-05-30 22:19:40 -0500

    taking score and for App to return score grade letter

--- script.js
+++ script.js
@@ -8,5 +8,11 @@ function getAverage(scores) {
   return average;
 }
 
+function getGrade(score) {
+    if (score <= 100) {
+      console.log("A++");
+      else (score <= 99) {
+        console.log("A");
+
 console.log(getAverage([92, 88, 12, 77, 57, 100, 67, 38, 97, 89]));
 console.log(getAverage([45, 87, 98, 100, 86, 94, 67, 88, 94, 95]));
