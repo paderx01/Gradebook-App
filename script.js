bb8fc406cddfbe1a0b5efd2b9e9f1f97259df879
Pader Xiong <paderx26@gmail.com>
2024-06-03 21:21:09 -0500

    using if else function to get grade base off score

--- script.js
+++ script.js
@@ -11,8 +11,21 @@ function getAverage(scores) {
 function getGrade(score) {
     if (score <= 100) {
       console.log("A++");
-      else (score <= 99) {
+      else (score <= 94) {
         console.log("A");
+        else (score <= 89) {
+          console.log("B");
+          else (score <= 78) {
+            console.log("C");
+          }
+        }
+      }
+    }
+    }
+    
+    console.log(getGrade(96));
+    console.log(getGrade(82));
+    console.log(getGrade(56));
 
 console.log(getAverage([92, 88, 12, 77, 57, 100, 67, 38, 97, 89]));
 console.log(getAverage([45, 87, 98, 100, 86, 94, 67, 88, 94, 95]));
