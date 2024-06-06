32535f88c05a65e9847fc44da40899ecca115fc1
Pader Xiong <paderx26@gmail.com>
2024-06-05 21:53:09 -0500

    get grade letter by score

--- script.js
+++ script.js
@@ -20,13 +20,13 @@ function getAverage(scores) {
 function getGrade(score) {
   if (score === 100) {
     return "A++";
-  } else if (score >= 90 || score <= 99) {
+  } else if (score >= 90 && score <= 99) {
     return "A";
-  } else if (score >= 80 || score <= 89) {
+  } else if (score >= 80 && score <= 89) {
     return "B";
-  } else if (score <= 70 || score <= 79) {
+  } else if (score >= 70 && score <= 79) {
     return "C";
-  } else if (score <= 60 || score <= 69) {
+  } else if (score >= 60 && score <= 69) {
     return "D";
   } else {
     return "F";
