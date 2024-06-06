84a9beabc996f6d6c2cf99ad6d0e9a9e3112ffa3
Pader Xiong <paderx26@gmail.com>
2024-06-05 21:42:53 -0500

    testing multiple if and else

--- script.js
+++ script.js
@@ -8,24 +8,33 @@ function getAverage(scores) {
   return average;
 }
 
-ffunction getGrade(score) {
-    if (score <= 100) {
-      console.log("A++");
-    }
-      else if (score <= 94) {
-        console.log("A");
-      }
-        else if (score <= 89) {
-          console.log("B");
-        }
-          else {(score <= 78)
-            console.log("C");
-          }
-        }
-    
-    console.log(getGrade(96));
-    console.log(getGrade(82));
-    console.log(getGrade(56));
+function getAverage(scores) {
+  let sum = 0;
 
+  for (const score of scores) {
+    sum += score;
+  }
+
+  return sum / scores.length;
+}
+function getGrade(score) {
+  if (score === 100) {
+    return "A++";
+  } else if (score >= 90 || score <= 99) {
+    return "A";
+  } else if (score >= 80 || score <= 89) {
+    return "B";
+  } else if (score <= 70 || score <= 79) {
+    return "C";
+  } else if (score <= 60 || score <= 69) {
+    return "D";
+  } else {
+    return "F";
+  }
+}
+
+console.log(getGrade(96));
+console.log(getGrade(82));
+console.log(getGrade(56));
 console.log(getAverage([92, 88, 12, 77, 57, 100, 67, 38, 97, 89]));
 console.log(getAverage([45, 87, 98, 100, 86, 94, 67, 88, 94, 95]));
