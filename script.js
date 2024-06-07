ec3844a113a4ab7932a8ac79b2aa76a435b3236c
Pader Xiong <paderx26@gmail.com>
2024-06-06 21:55:09 -0500

    checking if student has passing grade

--- script.js
+++ script.js
@@ -38,3 +38,19 @@ console.log(getGrade(82));
 console.log(getGrade(56));
 console.log(getAverage([92, 88, 12, 77, 57, 100, 67, 38, 97, 89]));
 console.log(getAverage([45, 87, 98, 100, 86, 94, 67, 88, 94, 95]));
+
+
+function hasPassingGrade(score) {
+    if getGrade(score >=60) {
+      return true {
+      } else getGrade (score <= 60) {
+        return false
+      }
+    }
+    }
+   }
+   
+   
+   console.log(hasPassingGrade(100));
+   console.log(hasPassingGrade(53));
+   console.log(hasPassingGrade(87));
